35e951eae906baf96dc972ac2a36de54f4a8138f
Mohnish Thallavajhula <i@mohni.sh>
2014-10-17 07:00:09 -0700

    Remove code

---
+++ app/controllers/api/entries_controller.rb
@@ -0,0 +1,5 @@
+module Api
+  class EntriesController < BaseController
+
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,3 @@
 Copypaste::Application.routes.draw do
-
   resources :entries, only: [:new, :create, :show]
-  get ':id' => 'entries#show'
-
-  root 'entries#new'
-
 end
